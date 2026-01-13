0dd972d75361108862a3bf448cf17693326f68e3
miyapoe <kanon.spiri@gmail.com>
2026-01-13 20:02:02 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,15 @@
 # scraper.py
 import requests
 
-def fetch_race_json(date: str, stadium: int):
-    """
-    date: YYYYMMDD
-    stadium: 場コード（ここでは 1 固定）
-    """
-    url = f"https://boatraceopenapi.github.io/programs/v1/{date}.json"
-    r = requests.get(url, timeout=10)
-    r.raise_for_status()
-    return r.json()
+def fetch_race_json(race_date: str, stadium: int, race_no: int):
+    url = f"https://boatraceopenapi.github.io/programs/v1/{race_date}.json"
+    res = requests.get(url)
+    res.raise_for_status()
+    data = res.json()
+
+    races = data["programs"]
+    for r in races:
+        if r["race_stadium_number"] == stadium and r["race_number"] == race_no:
+            return r
+
+    return None
